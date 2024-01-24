d80dda6332b3520bee1f155b85ccc92427181563
Andre Santos <andrefelipe_santos@outlook.com>
2024-01-24 20:33:15 -0300

    Imlementa a classe UserService.

---
+++ API/src/services/user.service.ts
@@ -0,0 +1,58 @@
+import { PrismaClient, User } from "@prisma/client";
+import JWT from "../utils/jwt";
+
+export default class UserService {
+    private model: PrismaClient = new PrismaClient();
+
+    public async create(newUser: User) {
+        const created = await this.model.user.create({
+            data: newUser,
+        });
+
+        return created;
+    }
+
+    public async login(email: string, password: string) {
+        const user = await this.model.user.findUnique({
+            where: {
+                email,
+                password, 
+            }
+        });
+
+        const payload = {
+            id: user?.id,
+            name: user?.name,
+            email: user?.email,
+        }
+
+        const token = JWT.token(payload);
+
+        return token;
+    }
+
+    public async update(id: number, obj: User) {
+        const user = await this.model.user.findUnique({
+            where: { id, }
+        });
+
+        const userUpdated = await this.model.user.update({
+            where: { id, },
+            data: {
+                name: obj.name == '' ? user?.name : obj.name,
+                password: obj.password == '' ? user?.password : obj.password,
+                email: obj.email == '' ? user?.email : obj.email,
+            }
+        });
+
+        return userUpdated;
+    }
+
+    public async delete(id: number) {
+        const deleted = await this.model.user.delete({
+            where: { id, }
+        })
+
+        return deleted;
+    }
+}
