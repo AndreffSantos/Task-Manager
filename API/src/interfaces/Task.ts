20ade59e8fd2cc8759b76adb0873e685edd6f0cc
Andre Santos <andrefelipe_santos@outlook.com>
2024-01-26 11:52:02 -0300

    implementa a interface Task.

---
+++ API/src/interfaces/Task.ts
@@ -0,0 +1,9 @@
+import { z } from "zod"
+
+export const taskSchema = z.object({
+    userId: z.number(),
+    title: z.string().min(3),
+    description: z.string().max(100, 'O maximo de caracteres são 100').optional(),
+});
+
+export type Task = z.infer<typeof taskSchema>
