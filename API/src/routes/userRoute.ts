de13867c8d1a97da26ea65cdbbceb98471380f4c
Andre Santos <andrefelipe_santos@outlook.com>
2024-02-01 22:19:40 -0300

    Refatora UserRouter para fazer login em um post.

--- API/src/routes/userRoute.ts
+++ API/src/routes/userRoute.ts
@@ -5,7 +5,7 @@ const userRoute = Router();
 const controller = new UserController();
 
 userRoute.post('/', async (req, res) => controller.create(req, res));
-userRoute.get('/', async (req, res) => controller.login(req, res));
+userRoute.post('/login', async (req, res) => controller.login(req, res));
 userRoute.put('/:id', async (req, res) => controller.update(req, res));
 userRoute.delete('/:id', async (req, res) => controller.delete(req, res));
 
